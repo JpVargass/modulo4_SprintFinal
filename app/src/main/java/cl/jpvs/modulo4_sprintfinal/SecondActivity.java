91b9094953973e14cf0a820de89a4f8d8332c9cc
JpVargass <136398972+JpVargass@users.noreply.github.com>
2023-06-30 09:16:15 -0400

    boton linkedin

--- app/src/main/java/cl/jpvs/modulo4_sprintfinal/SecondActivity.java
+++ app/src/main/java/cl/jpvs/modulo4_sprintfinal/SecondActivity.java
@@ -2,13 +2,32 @@ package cl.jpvs.modulo4_sprintfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import cl.jpvs.modulo4_sprintfinal.databinding.ActivitySecondBinding;
 
 public class SecondActivity extends AppCompatActivity {
-
+private ActivitySecondBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        ActivitySecondBinding binding = ActivitySecondBinding.inflate(getLayoutInflater());
+       binding=ActivitySecondBinding.inflate((getLayoutInflater()));
+       setContentView(binding.getRoot());
+
+binding.btnLinkedin.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+         String linkedinUrl ="www.linkedin.com/in/juanpablovargassoto";
+         Uri uri = Uri.parse(linkedinUrl);
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
+    }
+});
+
+
     }
 }
